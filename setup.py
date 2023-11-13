c5151e2150269e79fa74858d7b5c9e929ec2fb3b
ossyman <uhussain0@gmail.com>
2023-11-13 22:23:36 +0100

    add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+
+
+setup(
+    name="DSSS_homework_2",
+    version="0.1.3",
+    description=" Testing my first python package",
+    author='osman hussain',
+    author_email='uhussain0@gmail.com',
+    url="https://github.com/ossyman/osman_dsss_homework_2.git",
+)
