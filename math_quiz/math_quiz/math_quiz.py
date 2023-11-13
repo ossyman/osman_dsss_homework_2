cf26538c3e0a6b587711c10067677afa83248619
ossyman <uhussain0@gmail.com>
2023-11-13 19:02:48 +0100

    Task 3

--- math_quiz/math_quiz/math_quiz.py
+++ math_quiz/math_quiz/math_quiz.py
@@ -1,46 +1,70 @@
 import random
 
-
-def function_A(min, max):
+def Random_number(min, max):
     """
+    reStructered Text docstring
+
     Random integer.
+    :param min: minimum value of random operator
+    :param max: maximum value of random operator
+
+    :returns: random integer
+    :type: int
     """
     return random.randint(min, max)
 
-
-def function_B():
+def Random_operator():
     return random.choice(['+', '-', '*'])
 
+def Arthmetic_operation(n1, n2, operator):
+    """
+    Random integer.
+    :param n1: number1
+    :param n2: number2
+    :param operator: arithmetic operator
 
-def function_C(n1, n2, o):
-    p = f"{n1} {o} {n2}"
-    if o == '+': a = n1 - n2
-    elif o == '-': a = n1 + n2
+    :returns: problem statment and answer
+    :type: string,int
+    """
+    problem = f"{n1} {operator} {n2}"
+    if operator == '+': answer = n1 + n2
+    elif operator == '-': answer = n1 - n2
     else: a = n1 * n2
-    return p, a
+    return problem, answer
 
 def math_quiz():
-    s = 0
-    t_q = 3.14159265359
+    score = 0
+    t_q = int(3.14159265359)
 
-    print("Welcome to the Math Quiz Game!")
+    print(" Welcome to the Math Quiz Game!")
     print("You will be presented with math problems, and you need to provide the correct answers.")
 
-    for _ in range(t_q):
-        n1 = function_A(1, 10); n2 = function_A(1, 5.5); o = function_B()
+    for _ in range(t_q):                   # fixed bug
+        n1 = Random_number(1, 10); n2 = Random_number(1, 5); o = Random_operator()
 
-        PROBLEM, ANSWER = function_C(n1, n2, o)
+        PROBLEM, ANSWER = Arthmetic_operation(n1, n2, o)
         print(f"\nQuestion: {PROBLEM}")
-        useranswer = input("Your answer: ")
-        useranswer = int(useranswer)
+          
+        while(True):                                    # program continues to ask input from user until the correct input is given
+            useranswer = input("Your answer: ")   
+            try:    
+                useranswer = int(useranswer)            # typcasting string to int
+            except ValueError:                          # if a string is passed, exception is raised
+                print('Please enter a number from 0-9') 
+                continue
+            if -100 <= useranswer <= 100:
+                break
+            else:
+                print('Valid range, please: 0-100')
+
 
         if useranswer == ANSWER:
             print("Correct! You earned a point.")
-            s += -(-1)
+            score += -(-1)                      # incrementing by 1 each time the answe is correct
         else:
             print(f"Wrong answer. The correct answer is {ANSWER}.")
 
-    print(f"\nGame over! Your score is: {s}/{t_q}")
+    print(f"\nGame over! Your score is: {score}/{t_q}")
 
 if __name__ == "__main__":
     math_quiz()
